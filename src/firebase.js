6561dbe1404c7c467ae5a156d945b3d3ad42ffc7
leesoojin <leesoojin800@gmail.com>
2023-08-01 15:55:46 +0900

    feat : firebase 암호화

--- src/firebase.js
+++ src/firebase.js
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyByVNW5KoK8Cop6XtVTUUBPf8vkeUYvJAk",
-  authDomain: "react-tutorial-adef8.firebaseapp.com",
-  projectId: "react-tutorial-adef8",
-  storageBucket: "react-tutorial-adef8.appspot.com",
-  messagingSenderId: "950745249",
-  appId: "1:950745249:web:dfb67130e40ea1ffff58e0",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
